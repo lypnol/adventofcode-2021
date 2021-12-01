8d51d044cb967e29348755e448c44f0bfaee5105
Julien Doutre <jul.doutre@gmail.com>
2021-12-01 13:13:29 +0100

    Day 1

---
+++ day-01/part-1/juliendoutre.rs
@@ -0,0 +1,44 @@
+use std::env::args;
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+    let output = run(&args().nth(1).expect("Please provide an input"));
+    let elapsed = now.elapsed();
+    println!("_duration:{}", elapsed.as_secs_f64() * 1000.);
+    println!("{}", output);
+}
+
+fn run(input: &str) -> u64 {
+    let sliding_window_size = 1;
+
+    let measurements: Vec<u64> = input
+        .lines()
+        .map(|line| line.parse::<u64>().unwrap())
+        .collect();
+
+    (0..measurements.len() - sliding_window_size).fold(0, |increases, index| {
+        increases + (measurements[index] < measurements[index + sliding_window_size]) as u64
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        let test_case = "199
+200
+208
+210
+200
+207
+240
+269
+260
+263";
+
+        assert_eq!(run(test_case), 7)
+    }
+}
