1ab18a2a02db5790c5319a363cbc377e3ea8cd49
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-12-01 21:53:45 +0100

    [day-01] remove useless variable

--- day-01/part-1/badouralix.sh
+++ day-01/part-1/badouralix.sh
@@ -7,11 +7,10 @@ WINDOW=1
 function run() {
     # Your code goes here
     read -r -d '' -a ARRAY <<< "$INPUT"
-    BEFORE=("${ARRAY[@]:0:${#ARRAY[@]}-$WINDOW}")
-    AFTER=("${ARRAY[@]:$WINDOW}")
+    SHIFT=("${ARRAY[@]:$WINDOW}")
 
     # Return answer
-    wc -l <<< "$(for idx in "${!AFTER[@]}"; do if [ "${AFTER[$idx]}" -gt "${BEFORE[$idx]}" ]; then echo yolo; fi; done)"
+    wc -l <<< "$(for idx in "${!SHIFT[@]}"; do if [ "${SHIFT[$idx]}" -gt "${ARRAY[$idx]}" ]; then echo yolo; fi; done)"
 }
 
 echo "$(run)"
