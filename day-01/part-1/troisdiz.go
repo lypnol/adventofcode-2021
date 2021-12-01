a6489e6d0dba28c86a97a9b7ce9302c369ad557c
Denis Rampnoux <troisdiz@users.noreply.github.com>
2021-12-01 12:00:49 +0100

    troidiz - day-01 - part-01

---
+++ day-01/part-1/troisdiz.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+func parseData(input string) []int {
+
+	var data []int
+
+	for _, line := range strings.Split(input, "\n") {
+		parsed, _ := strconv.Atoi(line)
+		data = append(data, parsed)
+	}
+
+	return data
+}
+
+func puzzle(input []int) int {
+	var count int = 0
+	for i := 1; i < len(input); i++ {
+		if input[i] > input[i-1] {
+			count++
+		}
+	}
+	return count
+}
+
+func run(s string) interface{} {
+	// Your code goes here
+	data := parseData(s)
+	result := puzzle(data)
+	return result
+}
+
+func main() {
+	// Uncomment this line to disable garbage collection
+	// debug.SetGCPercent(-1)
+
+	argsWithProg := os.Args
+	var input []byte
+	var err error
+	if len(argsWithProg) > 1 {
+		input,err = ioutil.ReadFile(argsWithProg[1])
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		// Read input from stdin
+		input, err = ioutil.ReadAll(os.Stdin)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Start resolution
+	start := time.Now()
+	result := run(string(input))
+
+	// Print result
+	fmt.Printf("_duration:%f\n", time.Now().Sub(start).Seconds()*1000)
+	fmt.Println(result)
+}
