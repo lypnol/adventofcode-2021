d409f5775dd6b9265f669d623f15e404055e9adc
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-12-01 21:48:10 +0100

    [day-01] use better language for badouralix

---
+++ day-01/part-2/badouralix.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Access input string with $INPUT
+INPUT=$1
+WINDOW=4
+
+function run() {
+    # Your code goes here
+    read -ra BEFORE <<< "$(echo "$INPUT" | tr '\n' ' ' | rev | cut -d' ' -f$WINDOW- | rev)"
+    read -ra AFTER <<< "$(echo "$INPUT" | tr '\n' ' ' | cut -d' ' -f$WINDOW-)"
+
+    # Return answer
+    wc -l <<< "$(for idx in "${!AFTER[@]}"; do if [ "${AFTER[$idx]}" -gt "${BEFORE[$idx]}" ]; then echo yolo; fi; done)"
+}
+
+echo "$(run)"
