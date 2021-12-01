b5429b19801083a467916d6ebe0b17ba6fc1c314
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-12-01 21:50:30 +0100

    [day-01] boost badouralix bash solution

--- day-01/part-1/badouralix.sh
+++ day-01/part-1/badouralix.sh
@@ -2,16 +2,16 @@
 
 # Access input string with $INPUT
 INPUT=$1
-WINDOW=2
+WINDOW=1
 
 function run() {
     # Your code goes here
-    read -ra BEFORE <<< "$(echo "$INPUT" | tr '\n' ' ' | rev | cut -d' ' -f$WINDOW- | rev)"
-    read -ra AFTER <<< "$(echo "$INPUT" | tr '\n' ' ' | cut -d' ' -f$WINDOW-)"
+    read -r -d '' -a ARRAY <<< "$INPUT"
+    BEFORE=("${ARRAY[@]:0:${#ARRAY[@]}-$WINDOW}")
+    AFTER=("${ARRAY[@]:$WINDOW}")
 
     # Return answer
     wc -l <<< "$(for idx in "${!AFTER[@]}"; do if [ "${AFTER[$idx]}" -gt "${BEFORE[$idx]}" ]; then echo yolo; fi; done)"
-    echo
 }
 
 echo "$(run)"
--- day-01/part-2/badouralix.sh
+++ day-01/part-2/badouralix.sh
@@ -2,12 +2,13 @@
 
 # Access input string with $INPUT
 INPUT=$1
-WINDOW=4
+WINDOW=3
 
 function run() {
     # Your code goes here
-    read -ra BEFORE <<< "$(echo "$INPUT" | tr '\n' ' ' | rev | cut -d' ' -f$WINDOW- | rev)"
-    read -ra AFTER <<< "$(echo "$INPUT" | tr '\n' ' ' | cut -d' ' -f$WINDOW-)"
+    read -r -d '' -a ARRAY <<< "$INPUT"
+    BEFORE=("${ARRAY[@]:0:${#ARRAY[@]}-$WINDOW}")
+    AFTER=("${ARRAY[@]:$WINDOW}")
 
     # Return answer
     wc -l <<< "$(for idx in "${!AFTER[@]}"; do if [ "${AFTER[$idx]}" -gt "${BEFORE[$idx]}" ]; then echo yolo; fi; done)"
