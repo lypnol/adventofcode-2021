131aa301bfc394e399ecf0696d8cd744d2d83ee5
Ayoub Sbai <ayoub.sbai@datadoghq.com>
2021-12-02 13:34:17 +0100

    [day-02] ayoub cpp solution fix

--- day-02/part-1/ayoub.c
+++ day-02/part-1/ayoub.c
@@ -2,23 +2,23 @@
 #include <time.h>
 #include <stdlib.h>
 
-long long run(char* s) {
+long run(char* s) {
     size_t i = -1;
-    long long h = 0, d = 0;
+    long h = 0, d = 0;
 
     while (s[++i]) {
         if (s[i] == '\n') continue;
         if (s[i] == 'f') {
             i += 8;
-            h += (long long)(s[i]-'0');
+            h += (long)(s[i]-'0');
             i++;
         } else if (s[i] == 'u') {
             i += 3;
-            d -= (long long)(s[i]-'0');
+            d -= (long)(s[i]-'0');
             i++;
         } else if (s[i] == 'd') {
             i += 5;
-            d += (long long)(s[i]-'0');
+            d += (long)(s[i]-'0');
             i++;
         }
     }
@@ -33,10 +33,8 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    printf("sizeof: int=%lu long=%lu long long=%lu\n", sizeof(int), sizeof(long), sizeof(long long));
-
     clock_t start = clock();
-    long long answer = run(argv[1]);
+    long answer = run(argv[1]);
     
     printf("_duration:%f\n%lld\n", (float)( clock () - start ) * 1000.0 /  CLOCKS_PER_SEC, answer);
     return 0;
