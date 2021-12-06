0d9cc50f7ead8045fe530fbe55ec6771f2c9e5a3
Romain Mondon-Cancel <romain.mondoncancel@datadoghq.com>
2021-12-06 10:10:56 +0100

    Implement Python, day 6

---
+++ day-06/part-1/skasch.py
@@ -0,0 +1,47 @@
+
+import collections
+from typing import List
+from tool.runners.python import SubmissionPy
+
+
+DAYS = 80
+CYCLE = 7
+NEW = 2
+
+
+def parse(s: str) -> List[str]:
+    return s.split(",")
+
+
+class SkaschSubmission(SubmissionPy):
+    def run(self, s):
+        """
+        :param s: input in string format
+        :return: solution flag
+        """
+        # Your code goes here
+        cnt = collections.Counter([int(v) for v in parse(s)])
+        d = DAYS
+        while d >= CYCLE:
+            tmp_cnt = collections.Counter()
+            for v in range(CYCLE, CYCLE + NEW):
+                tmp_cnt[v - CYCLE] += cnt[v]
+                del cnt[v]
+            cnt += collections.Counter({v + 2: n for v, n in cnt.items()})
+            cnt += tmp_cnt
+            d -= CYCLE
+        return sum(n * (1 if v >= d else 2) for v, n in cnt.items())
+
+
+def test_skasch():
+    """
+    Run `python -m pytest ./day-06/part-1/skasch.py` to test the submission.
+    """
+    assert (
+        SkaschSubmission().run(
+            """
+3,4,3,1,2
+""".strip()
+        )
+        == 5934
+    )
