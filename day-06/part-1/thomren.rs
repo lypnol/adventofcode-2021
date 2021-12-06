50ddc931547815156175053aa8331b8feac4b4f1
Thomas Renault <thomas.renault56@gmail.com>
2021-12-06 09:01:52 +0100

    day 6

---
+++ day-06/part-1/thomren.rs
@@ -0,0 +1,40 @@
+use std::env::args;
+use std::time::Instant;
+
+fn main() {
+    let now = Instant::now();
+    let output = run(&args().nth(1).expect("Please provide an input"));
+    let elapsed = now.elapsed();
+    println!("_duration:{}", elapsed.as_secs_f64() * 1000.);
+    println!("{}", output);
+}
+
+fn run(input: &str) -> usize {
+    simulate(input, 80)
+}
+
+fn simulate(input: &str, n_days: usize) -> usize {
+    let mut fish_counts: [usize; 9] = [0; 9];
+    let mut zero_idx = 0;
+
+    for n in input.split(",") {
+        fish_counts[n.parse::<usize>().unwrap()] += 1
+    }
+
+    for _ in 0..n_days {
+        fish_counts[(zero_idx + 1 + 6) % 9] += fish_counts[zero_idx];
+        zero_idx = (zero_idx + 1) % 9;
+    }
+
+    fish_counts.iter().sum()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn run_test() {
+        assert_eq!(run("3,4,3,1,2"), 5934)
+    }
+}
