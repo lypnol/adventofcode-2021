0ea1258db727b93be97798e7189e85e51f8e9e97
Romain Mondon-Cancel <romain.mondoncancel@datadoghq.com>
2021-12-13 18:36:44 +0100

    Add memoization

--- day-12/part-2/skasch.py
+++ day-12/part-2/skasch.py
@@ -1,4 +1,5 @@
 import collections
+import functools
 from typing import DefaultDict, Set
 
 from tool.runners.python import SubmissionPy
@@ -19,36 +20,38 @@ END = "end"
 
 
 class SkaschSubmission(SubmissionPy):
+    @functools.lru_cache(None)
+    def dfs(self, position: str, visited: int, visited_twice: bool) -> int:
+        res = 0
+        for next_pos in self.graph[position]:
+            bpos = self.bits[next_pos]
+            if visited_twice and bpos & visited:
+                continue
+            if next_pos == START:
+                continue
+            if next_pos == END:
+                res += 1
+                continue
+            next_visited = visited
+            if bpos & self.small_caves:
+                next_visited |= bpos
+            res += self.dfs(next_pos, next_visited, visited_twice or (bpos & visited))
+        return res
+
     def run(self, s: str) -> int:
         """
         :param s: input in string format
         :return: solution flag
         """
         # Your code goes here
-        graph = parse(s)
-        small_caves = {cave for cave in graph if cave == cave.lower()}
-        # State: (position, visited, visited_twice)
-        # visited_twice is True if one small case has already been visited twice
-        stack = [(START, set(), False)]
-        res = 0
-        while stack:
-            (
-                position,
-                visited,
-                visited_twice,
-            ) = stack.pop()
-            for next_pos in graph[position]:
-                if visited_twice and next_pos in visited or next_pos == START:
-                    continue
-                if next_pos == END:
-                    res += 1
-                    continue
-                next_visited = {next_pos} if next_pos in small_caves else set()
-                stack.append((next_pos, visited | next_visited, visited_twice or next_pos in visited))
-        return res
+        self.graph = parse(s)
+        self.bits = {cave: 1 << idx for idx, cave in enumerate(self.graph)}
+        self.small_caves = sum(self.bits[cave] for cave in self.graph if cave == cave.lower())
+        self.dfs.cache_clear()
+        return self.dfs(START, 0, False)
 
 
-def test_skasch():
+def test_skasch() -> None:
     """
     Run `python -m pytest ./day-12/part-2/skasch.py` to test the submission.
     """
