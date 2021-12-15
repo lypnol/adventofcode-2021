a716fc30078e045bb014358e16acc6e7e5d8aa29
Cédric Talbot <cedtalbot@yahoo.fr>
2021-12-15 13:14:35 +0100

    Frenki day 15

---
+++ day-15/part-2/frenki.py
@@ -0,0 +1,58 @@
+from tool.runners.python import SubmissionPy
+
+class FrenkiSubmission(SubmissionPy):
+  def run(self,s):
+    d = s.splitlines()
+    m = len(d)
+    n = len(d[0])
+    w = [[int(d[i][j]) for j in range(n)] for i in range(m)]
+    v = [[0 for _ in range(5*n)] for _ in range(5*m)]
+    L = [[0 for _ in range(5*n)] for _ in range(5*m)]
+
+    for i in range(m):
+      for j in range(n):
+        r = w[i][j]
+        for k in range(5):
+          for l in range(5):
+            p = r + k + l
+            if p > 9:
+              p -= 9
+            v[k * m + i][l * n + j] = p
+    
+    m = 5*m
+    n = 5*n
+    def correct(i,j,d,b,g,h):
+      if d and j < n-1:
+        r = L[i][j] + v[i][j+1]
+        if r < L[i][j+1]:
+          L[i][j+1] = r
+          correct(i,j+1, True, True, False, True)
+      if g and j > 0:
+        r = L[i][j] + v[i][j-1]
+        if r < L[i][j-1]:
+          L[i][j-1] = r
+          correct(i,j-1, False, True, True, True)
+      if b and i < m-1:
+        r = L[i][j] + v[i+1][j]
+        if r < L[i+1][j]:
+          L[i+1][j] = r
+          correct(i+1,j, True, True, True, False)
+      if h and i > 0:
+        r = L[i][j] + v[i-1][j]
+        if r < L[i-1][j]:
+          L[i-1][j] = r
+          correct(i-1,j, True, False, True, True)
+
+
+    L[-1][-1] = v[-1][-1]
+    for i in range(m-2, -1, -1):
+      L[i][-1] = v[i][-1] + L[i+1][-1]
+    for j in range(n-2, -1, -1):
+      L[-1][j] = v[-1][j] + L[-1][j+1]
+
+    for i in range(m-2, -1, -1):
+      for j in range(n-2, -1, -1):
+        L[i][j] = v[i][j] + min(L[i+1][j], L[i][j+1])
+        correct(i,j, True, True, False, False)
+
+    return L[0][0] - v[0][0]
