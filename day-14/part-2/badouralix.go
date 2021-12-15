bc85ca8583451fff523bf467a79b6eeccad663bd
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-12-15 02:14:35 +0100

    [day-14] use byte arrays instead of strings

--- day-14/part-2/badouralix.go
+++ day-14/part-2/badouralix.go
@@ -11,20 +11,20 @@ import (
 
 const iterations = 40
 
-func rec(p string, n int, mapping map[string]string, cache map[string]map[int]map[string]int) map[string]int {
+func rec(p string, n int, mapping map[[2]byte]string, cache map[string]map[int]map[byte]int) map[byte]int {
 	if _, ok := cache[p]; !ok {
-		cache[p] = make(map[int]map[string]int)
+		cache[p] = make(map[int]map[byte]int)
 	}
 
 	if _, ok := cache[p][n]; ok {
 		return cache[p][n]
 	}
 
-	cache[p][n] = make(map[string]int)
+	cache[p][n] = make(map[byte]int)
 
 	if n == 0 {
 		for _, c := range p {
-			cache[p][n][string(c)]++
+			cache[p][n][byte(c)]++
 		}
 		return cache[p][n]
 	}
@@ -33,7 +33,7 @@ func rec(p string, n int, mapping map[string]string, cache map[string]map[int]ma
 		for k, v := range rec(p[0:2], n, mapping, cache) {
 			cache[p][n][k] += v
 		}
-		cache[p][n][string(p[1])]--
+		cache[p][n][p[1]]--
 		for k, v := range rec(p[1:], n, mapping, cache) {
 			cache[p][n][k] += v
 		}
@@ -41,11 +41,11 @@ func rec(p string, n int, mapping map[string]string, cache map[string]map[int]ma
 		return cache[p][n]
 	}
 
-	for k, v := range rec(p[0:1]+mapping[p], n-1, mapping, cache) {
+	for k, v := range rec(p[0:1]+mapping[[2]byte{p[0], p[1]}], n-1, mapping, cache) {
 		cache[p][n][k] += v
 	}
-	cache[p][n][mapping[p]]--
-	for k, v := range rec(mapping[p]+p[1:], n-1, mapping, cache) {
+	cache[p][n][mapping[[2]byte{p[0], p[1]}][0]]--
+	for k, v := range rec(mapping[[2]byte{p[0], p[1]}]+p[1:], n-1, mapping, cache) {
 		cache[p][n][k] += v
 	}
 
@@ -54,15 +54,15 @@ func rec(p string, n int, mapping map[string]string, cache map[string]map[int]ma
 
 func run(s string) int {
 	// Your code goes here
-	mapping := make(map[string]string)
-	cache := make(map[string]map[int]map[string]int)
+	mapping := make(map[[2]byte]string)
+	cache := make(map[string]map[int]map[byte]int)
 	min, max := math.MaxInt64, 0
 
 	split := strings.Split(s, "\n\n")
 
 	for _, line := range strings.Split(split[1], "\n") {
 		rule := strings.Split(line, " -> ")
-		mapping[rule[0]] = rule[1]
+		mapping[[2]byte{rule[0][0], rule[0][1]}] = rule[1]
 	}
 
 	for _, v := range rec(split[0], iterations, mapping, cache) {
