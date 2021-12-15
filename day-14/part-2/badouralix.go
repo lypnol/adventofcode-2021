91eb04fc7d011ce1d22c2027d1d923afe63471dc
Ayaz Badouraly <badouralix@users.noreply.github.com>
2021-12-15 12:42:45 +0100

    [day-14] add badouralix alt solution

--- day-14/part-2/badouralix.go
+++ day-14/part-2/badouralix.go
@@ -11,41 +11,28 @@ import (
 
 const iterations = 40
 
-func rec(p string, n int, mapping map[[2]byte]string, cache map[string]map[int]map[byte]int) map[byte]int {
+type Pair [2]byte
+
+func rec(p Pair, n int, mapping map[Pair][2]Pair, cache map[Pair]map[int]map[Pair]int) map[Pair]int {
 	if _, ok := cache[p]; !ok {
-		cache[p] = make(map[int]map[byte]int)
+		cache[p] = make(map[int]map[Pair]int)
 	}
 
 	if _, ok := cache[p][n]; ok {
 		return cache[p][n]
 	}
 
-	cache[p][n] = make(map[byte]int)
+	cache[p][n] = make(map[Pair]int)
 
 	if n == 0 {
-		for _, c := range p {
-			cache[p][n][byte(c)]++
-		}
+		cache[p][n][p]++
 		return cache[p][n]
 	}
 
-	if len(p) > 2 {
-		for k, v := range rec(p[0:2], n, mapping, cache) {
-			cache[p][n][k] += v
-		}
-		cache[p][n][p[1]]--
-		for k, v := range rec(p[1:], n, mapping, cache) {
-			cache[p][n][k] += v
-		}
-
-		return cache[p][n]
-	}
-
-	for k, v := range rec(p[0:1]+mapping[[2]byte{p[0], p[1]}], n-1, mapping, cache) {
+	for k, v := range rec(mapping[p][0], n-1, mapping, cache) {
 		cache[p][n][k] += v
 	}
-	cache[p][n][mapping[[2]byte{p[0], p[1]}][0]]--
-	for k, v := range rec(mapping[[2]byte{p[0], p[1]}]+p[1:], n-1, mapping, cache) {
+	for k, v := range rec(mapping[p][1], n-1, mapping, cache) {
 		cache[p][n][k] += v
 	}
 
@@ -54,18 +41,30 @@ func rec(p string, n int, mapping map[[2]byte]string, cache map[string]map[int]m
 
 func run(s string) int {
 	// Your code goes here
-	mapping := make(map[[2]byte]string)
-	cache := make(map[string]map[int]map[byte]int)
+	mapping := make(map[Pair][2]Pair)
+	cache := make(map[Pair]map[int]map[Pair]int)
 	min, max := math.MaxInt64, 0
 
 	split := strings.Split(s, "\n\n")
+	pattern := split[0]
 
 	for _, line := range strings.Split(split[1], "\n") {
 		rule := strings.Split(line, " -> ")
-		mapping[[2]byte{rule[0][0], rule[0][1]}] = rule[1]
+		mapping[Pair{rule[0][0], rule[0][1]}] = [2]Pair{Pair{rule[0][0], rule[1][0]}, Pair{rule[1][0], rule[0][1]}}
 	}
 
-	for _, v := range rec(split[0], iterations, mapping, cache) {
+	occurences := make(map[byte]int)
+	occurences[pattern[0]]++
+	occurences[pattern[len(pattern)-1]]++
+
+	for i := 0; i < len(pattern)-1; i++ {
+		for p, v := range rec(Pair{pattern[i], pattern[i+1]}, iterations, mapping, cache) {
+			occurences[p[0]] += v
+			occurences[p[1]] += v
+		}
+	}
+
+	for _, v := range occurences {
 		if v < min {
 			min = v
 		}
@@ -74,7 +73,7 @@ func run(s string) int {
 		}
 	}
 
-	return max - min
+	return (max - min) / 2
 }
 
 func main() {
