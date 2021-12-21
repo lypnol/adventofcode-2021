4e0b935d4f543d0f594345a765d078eb5ee4907d
Romain Mondon-Cancel <romain.mondoncancel@datadoghq.com>
2021-12-21 12:32:56 +0100

    Improve day 2

--- day-21/part-2/skasch.py
+++ day-21/part-2/skasch.py
@@ -1,5 +1,7 @@
+import collections
+import functools
 import re
-from typing import Iterable, List, Tuple
+from typing import DefaultDict, List, Tuple
 from tool.runners.python import SubmissionPy
 
 REGEX = re.compile(r"Player [1-2] starting position: ([0-9]+)")
@@ -19,22 +21,12 @@ BOARD_SIZE = 10
 MAX_SCORE = 21
 
 
-def move(pos: int, throw: int) -> int:
-    return (pos + throw - 1) % BOARD_SIZE + 1
+@functools.lru_cache(None)
+def move(pos_min1: int, throw: int) -> int:
+    return (pos_min1 + throw) % BOARD_SIZE + 1
 
 
-def build_new_states() -> List[List[List[List[int]]]]:
-    return [[[[0] * MAX_SCORE for _ in range(MAX_SCORE)] for _ in range(BOARD_SIZE)] for _ in range(BOARD_SIZE)]
-
-
-def dirac_dice() -> Iterable[Tuple[int, int]]:
-    yield 3, 1
-    yield 4, 3
-    yield 5, 6
-    yield 6, 7
-    yield 7, 6
-    yield 8, 3
-    yield 9, 1
+DIRAC_DICE = [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
 
 
 class SkaschSubmission(SubmissionPy):
@@ -44,35 +36,33 @@ class SkaschSubmission(SubmissionPy):
         :return: solution flag
         """
         # Your code goes here
-        pos = parse(s)
-        states = build_new_states()
+        pos1, pos2 = parse(s)
+        states: DefaultDict[Tuple[int, int, int, int], int] = collections.defaultdict(int)
         won = [0, 0]
-        states[pos[0] - 1][pos[1] - 1][0][0] = 1
-        valid_states = {(pos[0] - 1, pos[1] - 1, 0, 0)}
+        states[pos1 - 1, pos2 - 1, 0, 0] = 1
+        valid_states = {(pos1 - 1, pos2 - 1, 0, 0)}
         player = 0
         while valid_states:
             new_valid_states = set()
-            new_states = build_new_states()
+            new_states: DefaultDict[Tuple[int, int, int, int], int] = collections.defaultdict(int)
             for pos1_min1, pos2_min1, score1, score2 in valid_states:
-                count = states[pos1_min1][pos2_min1][score1][score2]
-                for throw, n_throws in dirac_dice():
+                count = states[pos1_min1, pos2_min1, score1, score2]
+                for throw, n_throws in DIRAC_DICE:
                     if player == 0:
-                        pos1 = pos1_min1 + 1
-                        new_pos1 = move(pos1, throw)
+                        new_pos1 = move(pos1_min1, throw)
                         new_score1 = score1 + new_pos1
                         if new_score1 >= MAX_SCORE:
                             won[player] += n_throws * count
                         else:
-                            new_states[new_pos1 - 1][pos2_min1][new_score1][score2] += n_throws * count
+                            new_states[new_pos1 - 1, pos2_min1, new_score1, score2] += n_throws * count
                             new_valid_states.add((new_pos1 - 1, pos2_min1, new_score1, score2))
                     else:
-                        pos2 = pos2_min1 + 1
-                        new_pos2 = move(pos2, throw)
+                        new_pos2 = move(pos2_min1, throw)
                         new_score2 = score2 + new_pos2
                         if new_score2 >= MAX_SCORE:
                             won[player] += n_throws * count
                         else:
-                            new_states[pos1_min1][new_pos2 - 1][score1][new_score2] += n_throws * count
+                            new_states[pos1_min1, new_pos2 - 1, score1, new_score2] += n_throws * count
                             new_valid_states.add((pos1_min1, new_pos2 - 1, score1, new_score2))
             player = 1 - player
             states = new_states
