c072922f327d2d23b4d413fb4c73887a90ece809
skasch <romain.mondoncancel@gmail.com>
2021-12-25 10:45:56 +0100

    Improve part 1

--- day-25/part-1/skasch.py
+++ day-25/part-1/skasch.py
@@ -1,4 +1,6 @@
-from typing import Iterable, List, Optional, Tuple
+import functools
+import itertools
+from typing import Iterable, List, Tuple
 
 from tool.runners.python import SubmissionPy
 
@@ -20,29 +22,38 @@ def parse(s: str) -> Tuple[List[str], int, int]:
     return res, nrows, ncols
 
 
+@functools.lru_cache(None)
 def indexes(direction: str, nrows: int, ncols: int) -> Iterable[List[int]]:
+    res = []
     if direction == EAST_CUCUMBER:
         for row in range(nrows):
-            yield list(range(ncols * row, ncols * (row + 1)))
+            res.append(list(range(ncols * row, ncols * (row + 1))))
     else:
         for col in range(ncols):
-            yield list(range(col, nrows * ncols, ncols))
+            res.append(list(range(col, nrows * ncols, ncols)))
+    return res
 
 
-def move(board: List[str], nrows: int, ncols: int, direction: str) -> None:
+def move(board: List[str], nrows: int, ncols: int, direction: str) -> bool:
+    moved = False
     for row_pos in indexes(direction, nrows, ncols):
-        first_state = board[row_pos[0]]
-        state = board[row_pos[0]]
-        for idx, pos in enumerate(row_pos[1:]):
-            if state == direction and board[pos] == EMPTY:
-                state = board[pos]
-                board[pos] = direction
-                board[row_pos[idx]] = EMPTY
-            else:
-                state = board[pos]
-        if state == direction and first_state == EMPTY:
+        first_right = board[row_pos[0]]
+        first_left = board[row_pos[-1]]
+        skip = False
+        for left, right in zip(row_pos, row_pos[1:]):
+            if skip:
+                skip = False
+                continue
+            if board[left] == direction and board[right] == EMPTY:
+                board[right] = direction
+                board[left] = EMPTY
+                moved = True
+                skip = True
+        if first_left == direction and first_right == EMPTY:
             board[row_pos[0]] = direction
             board[row_pos[-1]] = EMPTY
+            moved = True
+    return moved
 
 
 def display(board: List[str], nrows: int, ncols: int) -> str:
@@ -53,30 +64,30 @@ def display(board: List[str], nrows: int, ncols: int) -> str:
 
 
 class SkaschSubmission(SubmissionPy):
-    def run(self, s):
+    def run(self, s: str) -> int:
         """
         :param s: input in string format
         :return: solution flag
         """
         # Your code goes here
         board, nrows, ncols = parse(s)
-        prev_board: Optional[str] = None
         moves = 0
-        while (board_str := "".join(board)) != prev_board:
-            moves += 1
-            prev_board = board_str
-            move(board, nrows, ncols, EAST_CUCUMBER)
-            move(board, nrows, ncols, SOUTH_CUCUMBER)
+        for moves in itertools.count(1):
+            move1 = move(board, nrows, ncols, EAST_CUCUMBER)
+            move2 = move(board, nrows, ncols, SOUTH_CUCUMBER)
+            if not move1 and not move2:
+                break
         return moves
 
 
-def test_skasch():
+def test_skasch() -> None:
     """
     Run `python -m pytest ./day-25/part-1/skasch.py` to test the submission.
     """
     assert (
         SkaschSubmission().run(
-            """v...>>.vv>
+            """
+v...>>.vv>
 .vv>>.vv..
 >>.>v>...v
 >>v>>.>.v.
